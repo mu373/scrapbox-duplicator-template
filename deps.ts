f9ec235fb66b7189edefcb8c8f9d898614931872
Minami Ueda <minami.ueda@gmail.com>
2026-03-05 21:05:00 -0500

    revert scrapbox-userscript-std to 0.27.1 (0.31.0 requires import map)

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 import "jsr:@std/dotenv/load";
 export { assert, is } from "https://deno.land/x/unknownutil@v3.18.1/mod.ts";
-export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.31.0/rest/page-data.ts";
+export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.27.1/rest/page-data.ts";
